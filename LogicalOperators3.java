3b9850193536b3995003a14d4927289c7d90cfec
faishal <faishal2022cse@gmail.com>
2022-01-01 15:47:02 +0530

    Added Logicaloperators3

---
+++ LogicalOperators3.java
@@ -0,0 +1,16 @@
+class LogicalOperators3
+{
+  public static void main(String args[])
+  {
+    String a="faishal";
+    String b="Faishal";
+    if(a==b)
+    {
+    System.out.println("a not=  b");
+    }
+    else
+    {
+    System.out.println("a =  b");
+    }
+  }
+}
