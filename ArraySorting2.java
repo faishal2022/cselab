3082ffdfdaca3b51b73b6789398bfc198042db74
faishal <faishal2022cse@gmail.com>
2022-01-01 16:00:00 +0530

    AddedArraySorting2

---
+++ ArraySorting2.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+class ArraySorting2
+{
+	public static void main(String args[])
+	{
+	int[] a={10,9,8,7,6,5,4,3,2,1};
+        Arrays.sort(a);
+	for(int i=0;i<a.length;i++)
+	{	
+	 System.out.println(a[i]);
+	}
+	}
+}
