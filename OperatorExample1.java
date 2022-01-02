c8feded66094b617da5bc8fd053f67d2fef6e18d
faishal <faishal2022cse@gmail.com>
2022-01-02 09:26:02 +0530

    Added OperatorExample1

---
+++ OperatorExample1.java
@@ -0,0 +1,15 @@
+class OperatorExample1
+{
+
+public static void main(String args[])
+{
+int a,b;
+a=10;
+b=5;
+System.out.println("Addition a+b="+(a+b));
+System.out.println("Substraction a-b="+(a-b));
+System.out.println("multiply a*b="+(a*b));
+System.out.println("division a/b="+(a/b));
+System.out.println("modulo a%b="+(a%b));
+}
+} 
