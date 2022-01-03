affff108bc56245a056f71bfc232b8f28156cfc7
faishal <faishal2022cse@gmail.com>
2022-01-03 10:03:44 +0530

    AddedForloopExample

---
+++ ForloopExample.java
@@ -0,0 +1,11 @@
+class ForloopExample
+ {
+    public static void main(String args[])
+    {
+      int i;
+       for(i=1;i<11;i++)
+      {
+       System.out.println("faiz");
+      }
+    }
+ } 
